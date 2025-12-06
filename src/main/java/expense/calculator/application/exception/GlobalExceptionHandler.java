ac856a48c0a8530439c1d6b46891b15cb16589d8
Kapil Mankar <kapilmankar53@gmail.com>
2025-12-06 12:40:52 +0530

    changes

--- src/main/java/expense/calculator/application/exception/GlobalExceptionHandler.java
+++ src/main/java/expense/calculator/application/exception/GlobalExceptionHandler.java
@@ -1,10 +1,6 @@
 package expense.calculator.application.exception;
 
 import expense.calculator.application.error.ErrorResponse;
-import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.stream.Collectors;
-
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,53 +11,45 @@ import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
   @ExceptionHandler(MethodArgumentNotValidException.class)
-  public ResponseEntity<?> handleValidationExceptions(MethodArgumentNotValidException ex) {
-    // Get all validation errors
-    BindingResult result = ex.getBindingResult();
+  public ResponseEntity<ErrorResponse> handleValidationExceptions(
+      MethodArgumentNotValidException ex) {
     List<String> errorMessages =
-        result.getAllErrors().stream()
+        ex.getBindingResult().getAllErrors().stream()
             .map(
-                error -> {
-                  if (error instanceof FieldError fieldError) {
-                    return fieldError.getField() + ": " + error.getDefaultMessage();
-                  }
-                  return error.getDefaultMessage();
-                })
+                error ->
+                    error instanceof FieldError fieldError
+                        ? fieldError.getField() + ": " + error.getDefaultMessage()
+                        : error.getDefaultMessage())
             .collect(Collectors.toList());
 
-    // Return a custom error response
-    return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-        .body(new ErrorResponse("Validation failed", errorMessages));
+    return ResponseEntity.badRequest().body(new ErrorResponse("Validation failed", errorMessages));
   }
 
   @ExceptionHandler(NoSuchElementException.class)
-  public ResponseEntity<?> handleNoSuchElementException() {
-    String errorMessage = "No expense details found";
-
-    ErrorResponse errorResponse = new ErrorResponse("NOT_FOUND", List.of(errorMessage));
-
-    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+  public ResponseEntity<ErrorResponse> handleNoSuchElementException() {
+    return ResponseEntity.status(HttpStatus.NOT_FOUND)
+        .body(new ErrorResponse("NOT_FOUND", List.of("No expense details found")));
   }
 
   @ExceptionHandler(MissingServletRequestParameterException.class)
-  public ResponseEntity<?> handleMissingParameter() {
-    String errorMessage = "UUID is required but missing in the request.";
-
-    ErrorResponse errorResponse = new ErrorResponse("Bad Request", List.of(errorMessage));
-
-    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+  public ResponseEntity<ErrorResponse> handleMissingParameter() {
+    return ResponseEntity.badRequest()
+        .body(
+            new ErrorResponse(
+                "Bad Request", List.of("UUID is required but missing in the request.")));
   }
 
   @ExceptionHandler(EntityNotFoundException.class)
-  public ResponseEntity<?> handleEntityNotFound() {
-    String errorMessage = "Expense not found";
-
-    ErrorResponse errorResponse = new ErrorResponse("Bad Request", List.of(errorMessage));
-
-    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+  public ResponseEntity<ErrorResponse> handleEntityNotFound() {
+    return ResponseEntity.badRequest()
+        .body(new ErrorResponse("Bad Request", List.of("Expense not found")));
   }
 }
